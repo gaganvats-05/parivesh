f8821cc73fbd0e3350bb9518272f1434237e8e3f
Abhishek Gupta <abhishekgupta942001@gmail.com>
2022-11-05 23:35:17 +0530

    auth0 data added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,11 @@ import { store } from "./store/index";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider>
+  <Auth0Provider
+    domain="dev-4utpiwl7wai4et1g.us.auth0.com"
+    clientId="d2elecAI8jrBmxvGMyj8T91ia1eGt4EH"
+    redirectUri={window.location.origin}
+  >
     <Provider store={store}>
       <App />
     </Provider>
