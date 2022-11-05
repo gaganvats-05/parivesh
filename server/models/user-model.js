342c2280a4db1fc5869c557e4e92e37443476301
AmitThakur045 <at7129652@gmail.com>
2022-11-06 00:48:25 +0530

    changes in modal

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -3,12 +3,6 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
-    companyName: { type: String, required: true },
-    role: {
-      type: String,
-      required: true,
-    },
-    password: { type: String, required: true },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: "prod" }],
   },
   {
