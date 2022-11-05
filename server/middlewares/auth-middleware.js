034764a502e1ebd3a498984a99386faa28ed7056
AmitThakur045 <at7129652@gmail.com>
2022-11-05 16:49:22 +0530

    added middleware

---
+++ server/middlewares/auth-middleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    let decodedData;
+    if (token) {
+      decodedData = jwt.verify(token, "sEcReT");
+      req.userId = decodedData?.id;
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = auth;
